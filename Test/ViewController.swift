22980f55fad8386869181ddfb26114f850c686f8
Radu Istrati <radonciki@gmail.com>
2020-06-10 14:43:12 +0300

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Radu Istrati on 10.06.20.
+//  Copyright © 2020 Radu Istrati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
